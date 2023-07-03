107c49fdcc6ba881f38cf244928d6fcab30ed87d
Hy5ton <136145099+Hy5ton@users.noreply.github.com>
2023-07-03 09:46:00 +0400

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form111.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form111
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form111));
+            this.Re = new System.Windows.Forms.Button();
+            this.Mi = new System.Windows.Forms.Button();
+            this.Fa = new System.Windows.Forms.Button();
+            this.Sol = new System.Windows.Forms.Button();
+            this.La = new System.Windows.Forms.Button();
+            this.Si = new System.Windows.Forms.Button();
+            this.Dodiez = new System.Windows.Forms.Button();
+            this.Rediez = new System.Windows.Forms.Button();
+            this.Fadiez = new System.Windows.Forms.Button();
+            this.Soldiez = new System.Windows.Forms.Button();
+            this.Ladiez = new System.Windows.Forms.Button();
+            this.NOct = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.Restart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Do = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Re
+            // 
+            this.Re.Enabled = false;
+            this.Re.Location = new System.Drawing.Point(106, 135);
+            this.Re.Name = "Re";
+            this.Re.Size = new System.Drawing.Size(110, 303);
+            this.Re.TabIndex = 1;
+            this.Re.UseVisualStyleBackColor = true;
+            this.Re.Click += new System.EventHandler(this.Re_Click);
+            // 
+            // Mi
+            // 
+            this.Mi.Enabled = false;
+            this.Mi.Location = new System.Drawing.Point(209, 135);
+            this.Mi.Name = "Mi";
+            this.Mi.Size = new System.Drawing.Size(110, 303);
+            this.Mi.TabIndex = 2;
+            this.Mi.UseVisualStyleBackColor = true;
+            this.Mi.Click += new System.EventHandler(this.Mi_Click);
+            // 
+            // Fa
+            // 
+            this.Fa.Enabled = false;
+            this.Fa.Location = new System.Drawing.Point(314, 135);
+            this.Fa.Name = "Fa";
+            this.Fa.Size = new System.Drawing.Size(110, 303);
+            this.Fa.TabIndex = 3;
+            this.Fa.UseVisualStyleBackColor = true;
+            this.Fa.Click += new System.EventHandler(this.Fa_Click);
+            // 
+            // Sol
+            // 
+            this.Sol.Enabled = false;
+            this.Sol.Location = new System.Drawing.Point(418, 135);
+            this.Sol.Name = "Sol";
+            this.Sol.Size = new System.Drawing.Size(110, 303);
+            this.Sol.TabIndex = 4;
+            this.Sol.UseVisualStyleBackColor = true;
+            this.Sol.Click += new System.EventHandler(this.Sol_Click);
+            // 
+            // La
+            // 
+            this.La.Enabled = false;
+            this.La.Location = new System.Drawing.Point(523, 135);
+            this.La.Name = "La";
+            this.La.Size = new System.Drawing.Size(110, 303);
+            this.La.TabIndex = 5;
+            this.La.UseVisualStyleBackColor = true;
+            this.La.Click += new System.EventHandler(this.La_Click);
+            // 
+            // Si
+            // 
+            this.Si.Enabled = false;
+            this.Si.Location = new System.Drawing.Point(626, 135);
+            this.Si.Name = "Si";
+            this.Si.Size = new System.Drawing.Size(110, 303);
+            this.Si.TabIndex = 6;
+            this.Si.UseVisualStyleBackColor = true;
+            this.Si.Click += new System.EventHandler(this.Si_Click);
+            // 
+            // Dodiez
+            // 
+            this.Dodiez.Enabled = false;
+            this.Dodiez.Image = ((System.Drawing.Image)(resources.GetObject("Dodiez.Image")));
+            this.Dodiez.Location = new System.Drawing.Point(71, 135);
+            this.Dodiez.Name = "Dodiez";
+            this.Dodiez.Size = new System.Drawing.Size(75, 186);
+            this.Dodiez.TabIndex = 8;
+            this.Dodiez.UseVisualStyleBackColor = true;
+            this.Dodiez.Click += new System.EventHandler(this.Dodiez_Click);
+            // 
+            // Rediez
+            // 
+            this.Rediez.Enabled = false;
+            this.Rediez.Image = ((System.Drawing.Image)(resources.GetObject("Rediez.Image")));
+            this.Rediez.Location = new System.Drawing.Point(174, 135);
+            this.Rediez.Name = "Rediez";
+            this.Rediez.Size = new System.Drawing.Size(75, 186);
+            this.Rediez.TabIndex = 9;
+            this.Rediez.UseVisualStyleBackColor = true;
+            this.Rediez.Click += new System.EventHandler(this.Rediez_Click);
+            // 
+            // Fadiez
+            // 
+            this.Fadiez.Enabled = false;
+            this.Fadiez.Image = ((System.Drawing.Image)(resources.GetObject("Fadiez.Image")));
+            this.Fadiez.Location = new System.Drawing.Point(381, 135);
+            this.Fadiez.Name = "Fadiez";
+            this.Fadiez.Size = new System.Drawing.Size(75, 186);
+            this.Fadiez.TabIndex = 10;
+            this.Fadiez.UseVisualStyleBackColor = true;
+            this.Fadiez.Click += new System.EventHandler(this.Fadiez_Click);
+            // 
+            // Soldiez
+            // 
+            this.Soldiez.Enabled = false;
+            this.Soldiez.Image = ((System.Drawing.Image)(resources.GetObject("Soldiez.Image")));
+            this.Soldiez.Location = new System.Drawing.Point(486, 135);
+            this.Soldiez.Name = "Soldiez";
+            this.Soldiez.Size = new System.Drawing.Size(75, 186);
+            this.Soldiez.TabIndex = 11;
+            this.Soldiez.UseVisualStyleBackColor = true;
+            this.Soldiez.Click += new System.EventHandler(this.Soldiez_Click);
+            // 
+            // Ladiez
+            // 
+            this.Ladiez.Enabled = false;
+            this.Ladiez.Image = ((System.Drawing.Image)(resources.GetObject("Ladiez.Image")));
+            this.Ladiez.Location = new System.Drawing.Point(599, 135);
+            this.Ladiez.Name = "Ladiez";
+            this.Ladiez.Size = new System.Drawing.Size(75, 186);
+            this.Ladiez.TabIndex = 12;
+            this.Ladiez.UseVisualStyleBackColor = true;
+            this.Ladiez.Click += new System.EventHandler(this.Ladiez_Click);
+            // 
+            // NOct
+            // 
+            this.NOct.BackColor = System.Drawing.Color.White;
+            this.NOct.Image = ((System.Drawing.Image)(resources.GetObject("NOct.Image")));
+            this.NOct.Location = new System.Drawing.Point(365, 3);
+            this.NOct.Name = "NOct";
+            this.NOct.Size = new System.Drawing.Size(59, 55);
+            this.NOct.TabIndex = 15;
+            this.NOct.UseVisualStyleBackColor = false;
+            this.NOct.Click += new System.EventHandler(this.NOct_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Font = new System.Drawing.Font("SimSun", 30.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.maskedTextBox1.Location = new System.Drawing.Point(314, 3);
+            this.maskedTextBox1.Mask = "0";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(27, 54);
+            this.maskedTextBox1.TabIndex = 16;
+            this.maskedTextBox1.ValidatingType = typeof(int);
+            this.maskedTextBox1.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            // 
+            // Restart
+            // 
+            this.Restart.BackColor = System.Drawing.Color.White;
+            this.Restart.Image = ((System.Drawing.Image)(resources.GetObject("Restart.Image")));
+            this.Restart.Location = new System.Drawing.Point(430, 4);
+            this.Restart.Name = "Restart";
+            this.Restart.Size = new System.Drawing.Size(59, 55);
+            this.Restart.TabIndex = 17;
+            this.Restart.UseVisualStyleBackColor = false;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Sitka Banner", 20.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 39);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Выберите номер октавы";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Do
+            // 
+            this.Do.Enabled = false;
+            this.Do.Location = new System.Drawing.Point(1, 135);
+            this.Do.Name = "Do";
+            this.Do.Size = new System.Drawing.Size(110, 303);
+            this.Do.TabIndex = 19;
+            this.Do.UseVisualStyleBackColor = true;
+            this.Do.Click += new System.EventHandler(this.Do_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(523, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(190, 265);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form111
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(745, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.NOct);
+            this.Controls.Add(this.Ladiez);
+            this.Controls.Add(this.Soldiez);
+            this.Controls.Add(this.Fadiez);
+            this.Controls.Add(this.Rediez);
+            this.Controls.Add(this.Dodiez);
+            this.Controls.Add(this.Si);
+            this.Controls.Add(this.La);
+            this.Controls.Add(this.Sol);
+            this.Controls.Add(this.Fa);
+            this.Controls.Add(this.Mi);
+            this.Controls.Add(this.Re);
+            this.Controls.Add(this.Do);
+            this.Name = "Form111";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Re;
+        private System.Windows.Forms.Button Mi;
+        private System.Windows.Forms.Button Fa;
+        private System.Windows.Forms.Button Sol;
+        private System.Windows.Forms.Button La;
+        private System.Windows.Forms.Button Si;
+        private System.Windows.Forms.Button Dodiez;
+        private System.Windows.Forms.Button Rediez;
+        private System.Windows.Forms.Button Fadiez;
+        private System.Windows.Forms.Button Soldiez;
+        private System.Windows.Forms.Button Ladiez;
+        private System.Windows.Forms.Button NOct;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Do;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinFormsApp1/WinFormsApp1/Form111.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio;
+using NAudio.Midi;
+
+namespace WinFormsApp1
+{
+    public partial class Form111 : Form
+    {
+        int duration = 127;
+        int a, b, u, p;
+        int []oct = { 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23 };
+
+        public Form111()
+        {
+            InitializeComponent();
+        }
+        private readonly MidiOut midiOut = new MidiOut(0);
+        async Task Play()
+        {
+            await Task.Factory.StartNew(() => midiOut.Send(MidiMessage.StartNote(oct[u], duration, 1).RawData));
+        }
+
+        private void NOct_Click(object sender, EventArgs e) 
+        {
+            a = Int32.Parse(maskedTextBox1.Text);
+            if (a < 10)
+            {
+                for (b = a; b > 1; b--)
+                {
+                    for (int i = 11; i >= 0; i--)
+                    {
+                        oct[i] = oct[i] + 12;
+                    }
+                }
+                Do.Enabled = true;
+                Re.Enabled = true;
+                Mi.Enabled = true;
+                Fa.Enabled = true;
+                Sol.Enabled = true;
+                La.Enabled = true;
+                Si.Enabled = true;
+                Dodiez.Enabled = true;
+                Rediez.Enabled = true;
+                Fadiez.Enabled = true;
+                Soldiez.Enabled = true;
+                Ladiez.Enabled = true;
+                NOct.Enabled = false;
+                Do.BackColor = Color.White;
+                Re.BackColor = Color.White;
+                Mi.BackColor = Color.White;
+                Fa.BackColor = Color.White;
+                Sol.BackColor = Color.White;
+                La.BackColor = Color.White;
+                Si.BackColor = Color.White;
+            }
+            else MessageBox.Show("Такой октавы не существует");
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            
+        }
+
+        private void Do_Click_1(object sender, EventArgs e)
+        {
+            u = 0;
+            Task.Run(Play);
+        }
+
+        private void Re_Click(object sender, EventArgs e)
+        {
+            u = 2;
+            Task.Run(Play);
+        }
+
+        private void Mi_Click(object sender, EventArgs e)
+        {
+            u = 4;
+            Task.Run(Play);
+        }
+
+        private void Fa_Click(object sender, EventArgs e)
+        {
+            u = 5;
+            Task.Run(Play);
+        }
+
+        private void Sol_Click(object sender, EventArgs e)
+        {
+            u = 7;
+            Task.Run(Play);
+        }
+
+        private void La_Click(object sender, EventArgs e)
+        {
+            u = 9;
+            Task.Run(Play);
+        }
+
+        private void Si_Click(object sender, EventArgs e)
+        {
+            u = 11;
+            Task.Run(Play);
+        }
+
+        private void Dodiez_Click(object sender, EventArgs e)
+        {
+            u = 1;
+            Task.Run(Play);
+        }
+
+        private void Rediez_Click(object sender, EventArgs e)
+        {
+            u = 3;
+            Task.Run(Play);
+        }
+
+        private void Fadiez_Click(object sender, EventArgs e)
+        {
+            u = 6;
+            Task.Run(Play);
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            NOct.Enabled = true;
+            Do.Enabled = false;
+            Re.Enabled = false;
+            Mi.Enabled = false;
+            Fa.Enabled = false;
+            Sol.Enabled = false;
+            La.Enabled = false;
+            Si.Enabled = false;
+            Dodiez.Enabled = false;
+            Rediez.Enabled = false;
+            Fadiez.Enabled = false;
+            Soldiez.Enabled = false;
+            Ladiez.Enabled = false;
+            for (b = a; b > 1; b--)
+            {
+                for (int i = 0; i < 12; i++)
+                {
+                    oct[0] = 12;
+                    oct[i] = oct[i] + i;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (p == 0)
+            {
+                pictureBox1.Show();
+                p++;
+            }
+            else
+            {
+                pictureBox1.Visible = false;
+                p = 0;
+            }
+        }
+
+        private void Soldiez_Click(object sender, EventArgs e)
+        {
+            u = 8;
+            Task.Run(Play);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ladiez_Click(object sender, EventArgs e)
+        {
+            u = 10;
+            Task.Run(Play);
+        }
+
+        protected override bool ProcessCmdKey(ref Message message, Keys keyData)
+        {
+            if ((keyData & Keys.D) == Keys.D)
+            {
+                u = 0;
+                Task.Run(Play);
+            }
+            if((keyData & Keys.E) == Keys.E)
+            {
+                u = 1;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.S) == Keys.S)
+            {
+                u = 2;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.R) == Keys.R)
+            {
+                u = 3;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.F) == Keys.F)
+            {
+                u = 4;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.G) == Keys.G)
+            {
+                u = 5;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.Y) == Keys.Y)
+            {
+                u = 6;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.H) == Keys.H)
+            {
+                u = 7;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.U) == Keys.U)
+            {
+                u = 8;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.J) == Keys.J)
+            {
+                u = 9;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.I) == Keys.I)
+            {
+                u = 10;
+                Task.Run(Play);
+            }
+            if ((keyData & Keys.K) == Keys.K)
+            {
+                u = 11;
+                Task.Run(Play);
+            }
+            return base.ProcessCmdKey(ref message, keyData);
+        }
+    }
+}
